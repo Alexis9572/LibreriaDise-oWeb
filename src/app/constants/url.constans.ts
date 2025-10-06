b00e08b815d2fee21c10e88d3f5b43b2b1a17b92
Alexis9572 <torreshuamanialdairalexis@gmail.com>
2025-10-06 18:59:27 -0500

    NuevaVersion

--- src/app/constants/url.constans.ts
+++ src/app/constants/url.constans.ts
@@ -1,9 +1,9 @@
-export const dominio = "https://localhost:7093/"
+export const dominio = "https://localhost:7093/";
 
 export const UrlConstans = {
-    rol:dominio+'api/Rol',
-    producto:dominio+'api/Producto',
-    auth:dominio+'api/Auth',
-    pedido:dominio+'api/Pedido',
-    devolucion:dominio+'api/Devolucione'
-}
+    rol: dominio + 'api/Rol',
+    producto: dominio + 'api/Producto',
+    auth: dominio + 'api/Auth',
+    pedido: dominio + 'api/Pedido',
+    devolucion: dominio + 'api/Devolucione'
+};
